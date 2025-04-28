5775a17337c2373cb81c5199293f80e7b629f2f4
AydenHocking <hockinga2004@gmail.com>
2025-04-27 21:32:44 -0400

    feat(): added one simple pytest and updated docs/gitignore

---
+++ api/tests/test_ingredient.py
@@ -0,0 +1,24 @@
+from fastapi.testclient import TestClient
+import pytest
+from api.controllers import ingredient as controller
+from api.models import ingredient as model
+from api.main import app
+
+# Create a test client for the app
+client = TestClient(app)
+
+@pytest.fixture
+def db_session(mocker):
+     return mocker.Mock()
+def test_create_ingredient(db_session):
+    ingredient_data = {
+        "name": "PyTest_Ingredient",
+        "quantity": 100
+    }
+    ingredient_object = model.Ingredient(**ingredient_data)
+    created_ingredient = controller.create(db_session, ingredient_object)
+
+    # Assertions
+    assert created_ingredient is not None
+    assert created_ingredient.name == "PyTest_Ingredient"
+    assert created_ingredient.quantity == 100
