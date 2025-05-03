8293b8cba6524245117b8d1615b49b8b59a67abd
Alejandro <pmurillo@charlotte.edu>
2025-05-02 22:28:52 -0400

    Register search router in main application
    - Import search router module

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from .routers import index as indexRoute
+from .routers import search 
 from .models import model_loader
 from .dependencies.config import conf
 
